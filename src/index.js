ed12a1ffa3e8094d6defb1ffa7d1d05231bd9a77
prmiguel <miangel-pr@outlook.com>
2024-03-16 15:02:02 +0000

    Define PORT as env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { faker } = require('@faker-js/faker')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send(`Hello ${faker.person.firstName()}!`)
