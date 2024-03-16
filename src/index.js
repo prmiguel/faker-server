f4bc5a816957c196131d07af6c443dd84274bdcf
prmiguel <miangel-pr@outlook.com>
2024-03-16 14:46:13 +0000

    Add faker package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const { faker } = require('@faker-js/faker')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send(`Hello ${faker.person.firstName()}!`)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
