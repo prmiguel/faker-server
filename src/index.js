661575fe883e59b3d48a578e8bda820c0854bd87
prmiguel <miangel-pr@outlook.com>
2024-03-16 15:18:18 +0000

    Add a draft POST

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const { faker } = require('@faker-js/faker')
 const app = express()
+app.use(express.json());
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     const expression = 'faker.person.firstName()'
     const value = eval(expression)
     res.send(`Hello ${value}!`)
+})
+
+app.post("/", (req, res) => {
+    const data = req.body
+    res.send(data)
 })
 
 app.listen(port, () => {
